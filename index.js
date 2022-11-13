ac7968864aaad24a7d7d1f0538036082a6e1cc7b
M1 <m1@SeunghwanM1.local>
2022-11-14 01:18:22 +0900

    secret information setting

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+const config = require('./config/key')
+
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seunghwan:abcd1234@atlascluster.scdh2ae.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(config.mongoURI
 ).then(() => console.log('MongoDB Connected...'))
  .catch(err => console.log(err))
  
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('안녕하신가')
+})
+
+app.post('/register', (req, res) => {
+    // 회원 가입 시 필요한 정보들을 클라이언트에서 가져오면, 그것들을 DB에 넣어준다
+    // 그래서 유저 모델을 가져와야
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        //저장시 에러가 있다면, 클라이언트에 에러가 있다고 전달(json형식으로)
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({
+            // 성공 시에는, 성공했다고 전달
+            success: true
+        })
+    })
 })
 
 
--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = mongoose.Schema({
     email: {
         type: String,
         trim: true, // 트림 - 즉, 사이사이 공간 자동으로 없애줌
-        unique: true
+        unique: 1
     },
     password: {
         type: String,
@@ -19,7 +19,7 @@ const userSchema = mongoose.Schema({
         maxlength: 50
         },
     role:{ // 관리자, 유저 모드 구별위함 
-        type: number, // 유저 - 0 / 관리자 - 1 이런 식
+        type: Number, // 유저 - 0 / 관리자 - 1 이런 식
         default: 0
     },
     image:{
@@ -28,12 +28,12 @@ const userSchema = mongoose.Schema({
             type: String// 유효성 관리 가능
         },
         tokenExp:{
-            type: number // 토큰 사용기간 지정
+            type: Number // 토큰 사용기간 지정
         }
     }
 })
 
  const User = mongoose.model('User', userSchema)
  // 유저라는 모델이 userSchema 스키마를 감싼다
- module.exports = {User}
+ module.exports = { User }
  // 다른 파일에서도 유저 모델이 사용될 수 있게 export 해준다
