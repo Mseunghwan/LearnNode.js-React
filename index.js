1e0e58ac8cc78c9055bff39f48748024f37d4e97
M1 <m1@SeunghwanM1.local>
2022-11-14 10:28:11 +0900

    add compare password

--- index.js
+++ index.js
@@ -39,6 +39,26 @@ app.post('/register', (req, res) => {
     })
 })
 
+app.post('/login', (req, res) => {
+
+    // 요청된 이메일을 데이터베이스에서 있는지 찾는다
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user){ // 유저가 없다면
+            return res.json({
+                loginSucees: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다"
+            })
+        }
+    })
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        //isMatch는 비밀번호가 맞다는 거
+        
+    })
+    // 비밀번호까지 맞다면 토큰을 생성하기
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -32,6 +34,34 @@ const userSchema = mongoose.Schema({
         }
     }
 })
+
+userSchema.pre('save', function( next ){
+    var user = this; // this는 위 스키마 부분을 가리킴
+
+    if(user.isModified('password')){
+    // 비밀번호를 암호화시킨다
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        if(err) return next(err)
+        
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err)
+            user.password = hash
+            // 다음(index.js에서의 User.save())를 실행하는 next()
+            next()
+        })
+    })  
+    }  
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    // plainPassword 123456 
+    // 암호화된 비밀번호 $2b$10$5E9P.VyfneqH/.eXmTaPQeqIqoPTaXmdM2GLSnEYbxdasW3gUZOr2
+    // 암호화 체크 방법? plainPassword를 암호화해서 비교
+    bcrypt.compare(plainpassword, this.password, function(err, isMatch){
+        if(err) return cb(err), // 에러 있을 시 콜백
+        cb(null, isMatch) // 에러 없을 시 콜백, 에러는 null(없고), isMatch는 true값이 들어가 있을 것
+    })
+}
 
  const User = mongoose.model('User', userSchema)
  // 유저라는 모델이 userSchema 스키마를 감싼다
